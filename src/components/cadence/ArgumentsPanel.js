ed160467c8813a70eadcb0ad712abb2e51bfd355
Navid TehraniFar <navid@dapperlabs.com>
2022-09-01 16:31:17 -0700

    update key

--- src/components/cadence/ArgumentsPanel.js
+++ src/components/cadence/ArgumentsPanel.js
@@ -11,12 +11,12 @@ const Wrapper = styled.div`
 export default function ArgumentsPanel({ currentObject }) {
     return (
         <Wrapper>
-            {currentObject.arguments && currentObject.arguments.map(arg => (
+            {currentObject.arguments && currentObject.arguments.map((arg, idx) => (
                 <ArgumentInput
                     label={arg.name}
                     type={arg.type}
                     onChange={(v) => arg.value = v}
-                    key={currentObject.path + arg.name + arg.type}
+                    key={currentObject.path + arg.name + arg.type + idx}
                 />
             ))}
         </Wrapper>
